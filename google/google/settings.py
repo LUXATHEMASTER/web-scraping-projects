1a5a85899b96a4c8b60ab2b8020e37f9386e938c
wj lee <mymamyma@gmail.com>
2016-08-18 12:56:22 -0400

    google - item, pipeline, settings, googlespider updated

--- google/google/settings.py
+++ google/google/settings.py
@@ -14,12 +14,12 @@ BOT_NAME = 'google'
 SPIDER_MODULES = ['google.spiders']
 NEWSPIDER_MODULE = 'google.spiders'
 
-
+DEPTH_LIMIT = 1
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'google (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -64,9 +64,14 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'google.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'google.pipelines.GooglePipeline': 300,
+}
+
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "google"
+MONGODB_COLLECTION = "products"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
